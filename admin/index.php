1fd702183e4c272694e3f4550af896173cb23785
David Collinson <hiroto@hiroto.eu>
2013-01-10 22:50:53 +0000

    removed the table on changelog for fluid layout

--- admin/index.php
+++ admin/index.php
@@ -18,44 +18,28 @@
 <?php include("templates/sidebar.php"); ?>
                 </div>
                 <div class="span10">
-                    <div class="hero-unit">
-                        <table class="table">
-                            <thead>
-                                <th>Author</th>
-                                <th>Type</th>
-                                <th>Comment</th>
-                                <th>Private</th>
-                                <th>Update</th>
-                            </thead>
-                            <tbody>
 <?php
     foreach ($changes as $row) {
         switch ($row["type"]) {
         case 0: // add
-            $type = "<img src='/img/add.png'>"; break;
+            $type = "Added"; break;
         case 1: // fix
-            $type = "<img src='/img/fix.png'>"; break;
+            $type = "Fixed"; break;
         case 2: // fix
-            $type = "<img src='/img/del.png'>"; break;
+            $type = "Deleted"; break;
         default: // the hell?
             $type = "N/A"; break;
         }
-        $class_ = $row["major"] == 1 ? "error" : "";
-        $class_ = $row["private"] == 1 ? "info" : $class_;
+        $class_ = $row["major"] == 1 ? "text-error" : "";
+        $class_ = $row["private"] == 1 ? "text-info" : $class_;
         echo <<<CHANGE
-        <tr class="{$class_}">
-                    <td>{$row["name"]}</td>
-                    <td>{$type}</td>
-                    <td>{$row["comment"]}</td>
-                    <td>{$row["private"]}</td>
-                    <td><i class="icon-pencil"></i> <i class="icon-remove-circle"></i></td>
-        </tr>
+                    <div class="hero-unit">
+                        <h4>{$row["name"]} - {$type}<i class="icon-pencil"></i> <i class="icon-remove-circle"></i></h4>
+                        <p class="{$class_}">{$row["comment"]}</p>
+                    </div>
 CHANGE;
         }
 ?>
-                            </tbody>
-                        </table>
-                    </div>
                 </div>
             </div>
         <hr>
