b6cf2b91b44ef3ae460c599ad2b0d36da09cab11
David Collinson <hiroto@hiroto.eu>
2013-01-11 22:38:25 +0000

    added twitter callback

--- admin/callback/index.php
+++ admin/callback/index.php
@@ -1,3 +1,21 @@
 <?php
-
+    require_once("../private.php");
+    if (isset($_GET["oauth_token"])) {
+        $twitter->setToken($_GET["oauth_token"]);
+        $token = $twitter->getAccessToken();
+        $tok = $token->oauth_token;
+        $secret = $token->oauth_token_secret;
+        $twitter->setToken($tok, $secret);
+        $info = $twitter->get_accountVerify_credentials();
+        $info->response;
+        $db = new db();
+        if ($query = $db->prepare("INSERT INTO twitter (username, token, secret, active) VALUES (?, ?, ?, TRUE)")) {
+            $query->bind_param("sss", $info->screen_name, $tok, $secret);
+            $query->execute();
+            $query->close();
+        }
+        $db->close();
+    }
+    print_r($info->result);
+    //header("Location: /admin");
 ?>
